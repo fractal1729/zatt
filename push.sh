39d764fe4b4bdc9fe918044a6ae1d8aa81774ee2
Vinjai Vale <vinjai.vale@gmail.com>
2017-07-31 18:10:43 -0400

    auto-generated commit message

---
+++ push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git add .
+git commit -m "auto-generated commit message"
+git push
--- sync.sh
+++ sync.sh
@@ -4,3 +4,5 @@ git pull
 cd ..
 python3 setup.py install
 cd cluster
+rm -r storage.persist
+zattd --config zatt.json
