8393a63edfea73ea0dbc2f7df7a8a9462354bb1d
Vinjai Vale <vinjai.vale@gmail.com>
2017-07-31 17:40:20 -0400

    Added regularly spaced block test

---
+++ test.py
@@ -0,0 +1,12 @@
+from zatt.client import DistributedDict
+
+d = DistributedDict('97.107.138.108', 9000)
+
+def regularBlocks(blockSize, blockInterval, numBlocks): # blockSize units are 50KB, blockInterval ms
+	if numBlocks > 1000000: numBlocks = 1000000
+	for i in range(numBlocks):
+		preprefix = ('%06d'%i).encode("utf8")
+		blockunit = (("*".encode("utf8"))*16384)
+		for j in range(blockSize):
+			prefix = ('%04d'%j).encode("utf8")
+			d['block'+prefix] = preprefix+prefix+blockunit
