2b9da47720f82594f7f4541633a11633a7a3fe23
Vinjai Vale <vinjai.vale@gmail.com>
2017-08-01 02:37:47 -0400

    auto-generated commit message

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ from time import sleep
 
 d = DistributedDict('97.107.138.108', 9000)
 
-def regularBlocks(blockSize, blockInterval, numBlocks): # blockSize units are 50KB, blockInterval ms
-	if numBlocks > 1000000: numBlocks = 1000000
-	for i in range(numBlocks):
+def regularblks(blkSize, blkInterval, numblks, pieceDelay): # blkSize units are 50KB, blkInterval ms
+	if numblks > 1000000: numblks = 1000000
+	for i in range(numblks):
 		preprefix = ('%06d'%i).encode("utf8")
-		blockunit = (("*".encode("utf8"))*16384)
-		for j in range(blockSize):
+		blkunit = (("*".encode("utf8"))*16384)
+		for j in range(blkSize):
 			prefix = ('%04d'%j).encode("utf8")
-			d['block'+('%04d'%j)] = preprefix+"-".encode("utf8")+prefix+"-".encode("utf8")+blockunit
-			sleep(.02)
-		sleep(float(blockInterval)/1000.0)
+			d['blk'+('%04d'%j)] = preprefix+"-".encode("utf8")+prefix+"-".encode("utf8")+blkunit
+			sleep(float(pieceDelay)/1000.0)
+		sleep(float(blkInterval)/1000.0)
